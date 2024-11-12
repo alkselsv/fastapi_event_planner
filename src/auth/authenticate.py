79160fee093c92a174a7241564d145234b978564
Aleksey Eliseev <alekseyeliseev@ya.ru>
2024-11-12 22:04:57 +0300

    Revert "Integration tests (#3)"
    This reverts commit 3c1273bac845b4de1299ae2ca3cd0d05ec1b576d.

--- src/auth/authenticate.py
+++ src/auth/authenticate.py
@@ -1,17 +1,15 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from auth.jwt_handler import verify_access_token
 
-from auth.jwt_handler import JwtTokenHandler
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/signin")
 
 
-async def authenticate(
-    token: str = Depends(oauth2_scheme),
-) -> str:
+async def authenticate(token: str = Depends(oauth2_scheme)) -> str:
     if not token:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Sign in for access"
         )
-    decoded_token = JwtTokenHandler.verify_access_token(token)
+    decoded_token = verify_access_token(token)
     return decoded_token["user"]
